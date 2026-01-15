3b9619b870d283630b79a46f35fba7298b4b68db
matteo mengoli <matteo.mengoli@alum.udit.es>
2026-01-15 18:50:55 +0100

    Ultima correzione

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-// Seleziona tutte le immagini nella gallery
 const immagini = document.querySelectorAll('.container img');
 
 immagini.forEach(img => {
-    // Salviamo il percorso originale (bianco e nero)
     const srcOriginale = img.src; 
-    // Creiamo il percorso per quella colorata (aggiungendo _color prima dell'estensione)
     const srcColore = srcOriginale.replace('.png', 'COLORE.png');
-
-    // Pre-caricamento dell'immagine colorata per evitare scatti bianchi
     const tempImg = new Image();
     tempImg.src = srcColore;
 
-    // Quando il mouse entra
     img.addEventListener('mouseenter', () => {
         img.src = srcColore;
     });
-
-    // Quando il mouse esce
+    
     img.addEventListener('mouseleave', () => {
         img.src = srcOriginale;
     });
